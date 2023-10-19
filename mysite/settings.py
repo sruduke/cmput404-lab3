e8b7c4b472b11e4d51c7e80351ce31c502158589
Sophia Ruduke <sruduke@ualberta.ca>
2023-10-18 18:03:13 -0600

    updated settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 import django_on_heroku
 from pathlib import Path
 
-django_on_heroku.settings(locals())
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -122,3 +122,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+django_on_heroku.settings(locals())
